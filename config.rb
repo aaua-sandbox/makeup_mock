8e1d88e0d41072961a228bd8b242ea859e12a701
aaua-sandbox <aaua.tatsuya@gmail.com>
2016-06-18 22:07:44 +0900

    wip

--- config.rb
+++ config.rb
@@ -11,8 +11,9 @@ page '/*.txt', layout: false
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
-page "/admin/*", :layout => "admin"
-page "/user/*", :layout => "layout"
+page '/', layout: false
+page '/admin/*', layout: 'admin'
+page '/user/*', layout: 'layout'
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
